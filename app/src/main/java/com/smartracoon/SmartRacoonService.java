a2d0322243f4708a0ba1b46a21e223e268395f63
Benno Eigenmann <beigenmann@gmail.com>
2019-09-16 11:46:01 +0200

    init

--- app/src/main/java/com/smartracoon/SmartRacoonService.java
+++ app/src/main/java/com/smartracoon/SmartRacoonService.java
@@ -4,6 +4,8 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -11,6 +13,9 @@ import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import net.osmand.aidl.navigation.ADirectionInfo;
+import net.osmand.aidl.navigation.OnVoiceNavigationParams;
+
+import java.util.List;
 
 public class SmartRacoonService extends Service {
     private static final int REQUEST_CODE = 6333; // onActivityResult request
@@ -23,6 +28,10 @@ public class SmartRacoonService extends Service {
     private OsmAndAidlHelper mOsmAndAidlHelper;
     private OsmAndHelper.OnOsmandMissingListener mOnOsmandMissingListener;
     private long callback = 3423;
+    private int nofifyid = 3412;
+    private Bitmap mbm;
+    private String mdir;
+    private int mdistance = 0;
 
     @Override
     public void onCreate() {
@@ -33,37 +42,52 @@ public class SmartRacoonService extends Service {
 
             }
         };
+        mbm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_racoon);
 
         mOsmAndAidlHelper = new OsmAndAidlHelper(this, mOnOsmandMissingListener);
         mOsmAndHelper = new OsmAndHelper(this, REQUEST_CODE, mOnOsmandMissingListener);
+        mOsmAndAidlHelper.setVoiceRouterNotifyListener(new OsmAndAidlHelper.VoiceRouterNotifyListener() {
+
+            @Override
+            public void onVoiceRouterNotify(OnVoiceNavigationParams params) {
+                List<String> cmds = params.getPlayed();
+
+
+                if (cmds.size() > 0) {
+                    mdir = "";
+                    for (String s : cmds) {
+                        mdir = mdir + " " + s;
+                    }
+
+                }
+                if (mdistance > 0) {
+                    String dir = "Distance " + mdistance + " m " + mdir;
+                    Intent notificationIntent = new Intent(SmartRacoonService.this, MainActivity.class);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(SmartRacoonService.this,
+                            0, notificationIntent, 0);
+
+
+                    Notification notification = new NotificationCompat.Builder(SmartRacoonService.this, SmartRacoonApp.CHANNEL_ID)
+
+                            .setVisibility(Notification.VISIBILITY_PUBLIC)
+                            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+                            // .setContentTitle("Direction")
+                            .setContentTitle(dir)
+                            .setLargeIcon(mbm)
+                            .setSmallIcon(R.drawable.ic_racoon)
+                            .setContentIntent(pendingIntent)
+                            .build();
+                    startForeground(nofifyid, notification);
+                }
+
+            }
+        });
         mOsmAndAidlHelper.setNavigationInfoUpdateListener(new OsmAndAidlHelper.NavigationInfoUpdateListener() {
 
             @Override
             public void onNavigationInfoUpdate(ADirectionInfo directionInfo) {
-                int distance = directionInfo.getDistanceTo();
-                int turntype = directionInfo.getTurnType();
-                String direct ;
-                if(directionInfo.isLeftSide()){
-                    direct = "rechts";
-                }
-                else{
-                    direct = "links";
-                }
-                String dir = "Distance " + distance + "\n" + " " + turntype + " " + direct;
-                Intent notificationIntent = new Intent(SmartRacoonService.this, MainActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(SmartRacoonService.this,
-                        0, notificationIntent, 0);
-
-
-                Notification notification = new NotificationCompat.Builder(SmartRacoonService.this, SmartRacoonApp.CHANNEL_ID)
-                        .setContentTitle("Direction")
-                        .setContentText(dir)
-                        .setSmallIcon(R.drawable.ic_racoon)
-                        .setContentIntent(pendingIntent)
-                        .build();
-
-                startForeground(1, notification);
-
+                mdistance = directionInfo.getDistanceTo();
             }
         });
 
@@ -86,13 +110,14 @@ public class SmartRacoonService extends Service {
                 case ACTION_START_FOREGROUND_SERVICE:
                     String input = intent.getStringExtra(inputExtra);
                     notification = new NotificationCompat.Builder(this, SmartRacoonApp.CHANNEL_ID)
+                            .setVisibility(Notification.VISIBILITY_PUBLIC)
                             .setContentTitle("SmartRacoon")
-                            .setContentText(input)
                             .setSmallIcon(R.drawable.ic_racoon)
+                            .setLargeIcon(mbm)
                             .setContentIntent(pendingIntent)
                             .build();
 
-                    startForeground(1, notification);
+                    startForeground(nofifyid, notification);
                     Toast.makeText(getApplicationContext(), "Foreground service is started.", Toast.LENGTH_LONG).show();
 
                     break;
@@ -103,19 +128,21 @@ public class SmartRacoonService extends Service {
                 case ACTION_GPX_SERVICE:
                     Uri uri = intent.getParcelableExtra(inputGPX);
                     notification = new NotificationCompat.Builder(this, SmartRacoonApp.CHANNEL_ID)
+                            .setVisibility(Notification.VISIBILITY_PUBLIC)
                             .setContentTitle("SmartRacoon")
                             .setContentText(uri.getPath())
                             .setSmallIcon(R.drawable.ic_racoon)
+                            //.setLargeIcon(mbm)
                             .setContentIntent(pendingIntent)
                             .build();
 
-                    startForeground(1, notification);
+                    startForeground(nofifyid, notification);
                     //mOsmAndAidlHelper.navigateGpxFromUri(uri, true);
+                    mOsmAndAidlHelper.registerForVoiceRouterMessages(true, callback);
                     mOsmAndAidlHelper.registerForNavigationUpdates(true, callback);
-                    mOsmAndHelper.muteNavigation();
+                    //mOsmAndHelper.muteNavigation();
 
                     mOsmAndHelper.navigateGpxUri(true, uri);
-
 
 
                     break;
